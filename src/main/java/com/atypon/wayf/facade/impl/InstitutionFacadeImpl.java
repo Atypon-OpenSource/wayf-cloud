744e2ca26cab968f0e8a1e3a49d6c32659f6a663
Matthew Mason <mjm@matthewjmason.com>
2017-03-28 10:59:36 -0400

    Formatting tweaks

--- src/main/java/com/atypon/wayf/facade/impl/InstitutionFacadeImpl.java
+++ src/main/java/com/atypon/wayf/facade/impl/InstitutionFacadeImpl.java
@@ -30,31 +30,40 @@ public class InstitutionFacadeImpl implements InstitutionFacade {
     public Single<Institution> create(Institution institution) {
         LOG.debug("Creating institution [{}]", institution);
 
+        // Used to demonstrate error handling
         if (institution.getName().equals("error")) {
             throw new RuntimeException("Nice try");
         }
 
-        return Single.just(institution).observeOn(Schedulers.io()).map((o_institution) -> dao.create(o_institution));
+        return Single.just(institution)
+                .observeOn(Schedulers.io())
+                .map((o_institution) -> dao.create(o_institution));
     }
 
     @Override
     public Single<Institution> read(String id) {
         LOG.debug("Reading institution with id [{}]", id);
 
-        return Single.just(id).observeOn(Schedulers.io()).map((o_id) -> dao.read(o_id));
+        return Single.just(id)
+                .observeOn(Schedulers.io())
+                .map((o_id) -> dao.read(o_id));
     }
 
     @Override
     public Single<Institution> update(Institution institution) {
         LOG.debug("Updating institution [{}]", institution);
 
-        return Single.just(institution).observeOn(Schedulers.io()).map((o_institution) -> dao.update(o_institution));
+        return Single.just(institution)
+                .observeOn(Schedulers.io())
+                .map((o_institution) -> dao.update(o_institution));
     }
 
     @Override
     public Completable delete(String id) {
         LOG.debug("Reading institution with id [{}]", id);
 
-        return Single.just(id).observeOn(Schedulers.io()).flatMapCompletable((o_id) -> Completable.fromAction(() -> dao.delete(o_id)));
+        return Single.just(id)
+                .observeOn(Schedulers.io())
+                .flatMapCompletable((o_id) -> Completable.fromAction(() -> dao.delete(o_id)));
     }
 }
